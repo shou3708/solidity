c309c0df91a17b5e06dfb98854cae0b5b038c95c
shou3708 <80809664+shou3708@users.noreply.github.com>
2022-06-14 14:37:42 +0800

    Update call_method.sol

--- call_method.sol
+++ call_method.sol
@@ -7,25 +7,24 @@ contract class22{
     string public string_1;
 
     event setNumber(string _from);
-  // 事件  事件名稱  你要紀錄的東西
+  // 事件  
     
-    //pure 不讀鏈上資料 不改鏈上資料     計算東西...
+    //pure 不讀鏈上資料 不改鏈上資料     
     function function_1(uint a,uint b) public pure returns(uint256){
         return a + 2*b;
     }
     
-    //view 讀鏈上資料 不改鏈上資料   getName...
+    //view 讀鏈上資料 不改鏈上資料   
     function function_2() public view returns(uint256){
         return integer_1 + integer_2;
     }
 
-    //修改鏈上資料    setName...
+    //修改鏈上資料    
     function function_3(string x) public returns(string){
         string_1 = x;
         emit setNumber(string_1);
         return string_1;
     }
-    //實作事件12/29 第23堂課
     
     //修改鏈上資料
     function class(string x) public returns(string){
